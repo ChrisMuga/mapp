2a4c2d18b6037c120e9ed4f45f5dd8c1ed0b77c6
Chris Muga <chrismuga94@gmail.com>
2019-04-05 05:46:42 +0300

    navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -13,20 +13,26 @@ class Navigation extends Component {
     return (
         <Router>
             <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/countries">Countries</Link>
-                        </li>
-                        <li>
-                            <Link to="/map/">Map</Link>
-                        </li>
-                    </ul>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <Link className = "nav-link" to="/countries">Countries</Link>
+                            </li>
+                            <li className = "nav-item">
+                                <Link className = "nav-link" to="/map/">Map</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </nav>
-        
-                <Route path="/map" exact component={Map} />
-                <Route path="/countries/" component={Countries} />
             </div>
+        
+            <Route path="/map" exact component={Map} />
+            <Route path="/countries/" component={Countries} />
         </Router>
     )
   }
